34a8e563e4a39984eb0f5f7518d59839804c8999
Sifiso Shabalala <sifiso.shabalala10@gmail.com>
2020-08-26 22:54:55 +0200

    Initial commit

---
+++ Gate/Gate/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Gate.Services;
+using Gate.Views;
+
+namespace Gate
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
